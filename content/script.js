cc947f9ed5c1a94d660645426d90b598a849fca6
Thomas Le <thomas@noissue.co>
2021-07-31 19:11:27 +1200

    Add code

---
+++ content/script.js
@@ -0,0 +1,23 @@
+/*Sets the cursor depending on the selected theme.*/
+function runScript() {
+    var cursorURL = chrome.extension.getURL("images/dog-chichi.png")
+
+    //Cursor initally set to first cursor
+    document.body.style.cursor = "url('" + cursorURL + "'), auto";
+
+    /*Switches to a new cursor from the set every time the user clicks or drags mouse*/
+    document.addEventListener("click", () => {
+        enlargeImage()
+    });
+
+    var enlargeImage = () => {
+        console.log('Hey image, become larger')
+    }
+
+    /*Sets the styling of the cursor to the given URL*/
+    var setCursor = (url) => {
+        document.body.style.cursor = "url('" + url + "') 0 0, auto";
+    }
+}
+
+runScript()
